86f933f3302365dd4e00ec50a67d414c6a4b10dc
tthieman <travis.thieman@gmail.com>
2013-12-12 14:04:50 -0500

    Behold, LAZUBOT!

--- project.clj
+++ project.clj
@@ -3,4 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :main lazubot.core
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/tools.logging "0.2.6"]
+                 [org.clojure/core.async "0.1.262.0-151b23-alpha"]
+                 [clojure-zulip "0.1.0-SNAPSHOT"]
+                 [clojail "1.0.6"]])
--- src/lazubot/core.clj
+++ src/lazubot/core.clj
@@ -1,6 +1,51 @@
-(ns lazubot.core)
+(ns lazubot.core
+  (:require [clojure.java.io :as io]
+            [clojure.tools.logging :refer [warn]]
+            [clojure.core.async :as async]
+            [clojure-zulip.core :as zulip]
+            [clojail.core :refer [sandbox]]
+            [clojail.testers :refer [secure-tester]])
+  (:import [java.io PushbackReader]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def config (with-open [r (io/reader (io/resource "private/config"))]
+              (read (PushbackReader. r))))
+(def conn (zulip/connection config))
+(def bot-streams ["test-stream" "clojure" "ClojureScript Compiler"
+                  "code review" "commits" "Victory"])
+(def num-goroutines 10)
+(def sb (sandbox secure-tester))
+
+(defn ensure-subscriptions []
+  (zulip/sync* (zulip/add-subscriptions conn bot-streams)))
+
+(defn respond [message reply]
+  (let [parsed-reply (if (seq? reply) (str (seq reply)) (str reply))]
+    (if (= "private" (get-in message [:message :type]))
+      (zulip/send-private-message conn
+                                  (map :email (get-in message [:message :display_recipient]))
+                                  parsed-reply)
+      (zulip/send-stream-message conn
+                                 (get-in message [:message :display_recipient])
+                                 (get-in message [:message :subject])
+                                 parsed-reply))))
+
+(defn process-message [message]
+  (when (and (= "message" (:type message))
+             (not= (get-in message [:message :sender_email])
+                   (get-in conn [:opts :username])))
+    (let [content (get-in message [:message :content])]
+      (when (= (first "(") (first content))
+        (respond message (sb (read-string content)))))))
+
+(defn -main []
+  (ensure-subscriptions)
+  (let [queue-id (:queue_id (zulip/sync* (zulip/register conn ["message"])))
+        messages (zulip/subscribe-events conn queue-id)]
+    (dotimes [n num-goroutines]
+      (async/go (loop []
+                  (try
+                    (process-message (async/<! messages))
+                    (catch Exception e
+                      (warn e)))
+                  (recur)))))
+  (println "Lazubot listening for messages"))
