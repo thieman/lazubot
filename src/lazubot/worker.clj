3e4e5be38726a1f96801f2ddb0ccf75313c9aaea
tthieman <travis.thieman@gmail.com>
2013-12-19 11:40:03 -0500

    Clojail seems to be broken

--- src/lazubot/worker.clj
+++ src/lazubot/worker.clj
@@ -1,16 +1,13 @@
 (ns lazubot.worker
-  (:require [clojail.core :refer [sandbox]]
-            [clojure.core.async :refer [>!! <!! chan sliding-buffer]]
+  (:require [clojure.core.async :refer [>!! <!! chan sliding-buffer]]
             [com.keminglabs.zmq-async.core :refer [register-socket!]]
             [cheshire.core :as cheshire]))
-
-(def liberal-sandbox (sandbox []))
 
 (defn eval-form
   "Take in a Clojure form string. Return a string of the evaluation
   result."
   [form-string]
-  (str (liberal-sandbox (read-string form-string))))
+  (str (eval (read-string form-string))))
 
 (defn -main []
   (let [addr "tcp://eth0:8080"
